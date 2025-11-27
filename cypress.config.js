b08013f031492952a983148f98e7088a2ce74459
hamza.ksaier <hamzaksaier@takiacademyteam.com>
2025-11-27 14:36:21 +0100

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    videoFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+
     },
   },
 });
